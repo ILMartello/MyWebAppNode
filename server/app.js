3dfb1cbec0a04871752a0d115d2676c07d5dea5c
ILMartello <marcellomassidda@gmail.com>
2021-02-22 22:24:50 +0100

    JWT HS256

--- server/app.js
+++ server/app.js
@@ -1,23 +1,26 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
 const dotenv = require('dotenv');
 
-//For jwt
+//For session and db save
 const {v4 : uuidv4} = require('uuid');
 const session = require('express-session');
 var MySQLStore = require('express-mysql-session')(session);
 const mysql = require('mysql');
+//For jwt
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
+
+
+
+const app = express();
+app.use(cookieParser());
 dotenv.config();
+
 const dbService = require('./dbService');
 
-const connection = require('./dbService');
-
-
-
 //Middleware per session
-
 app.use(session({
     secret: process.env.SESSIONKEY,
     resave:false,
@@ -32,23 +35,27 @@ app.use(session({
         port: process.env.DB_PORT
     })
 }));
+//end middleware session
 
-//
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended : false }));
 
 
-//Test Conservazione dello stato e informazioni nel coockie
+
+//Test Conservazione dello stato e informazioni nel cookie
 app.get('/',(req,res)=>{
     req.session.colorepreferito = "blu";
     console.log(req.session.id);
     res.send();
-}); 
+});
+//Test finto login
+/*
 app.get('/colore',(req,res)=>{
     res.send(`il tuo colore preferito è ${req.session.colorepreferito}`);
 }); 
-//Test finto login
+
 app.get('/login', (req,res)=>{
     req.session.isLogged = true;
     res.send();
@@ -59,15 +66,41 @@ app.get('/postlogin',(req,res)=>{
     }
     else{res.send('non sei loggato');}
 }); 
-
 app.get('/logout', (req,res)=>{
    // req.session.isLogged = false;
     //oppure per distruggere il documento salvato
     req.session.destroy(err=>console.log(err));
     res.send();
 });
+*/
 
 
+
+
+app.get('/login', (req , res) =>{
+    const payload = {id:1, isLogged: true};
+    const options = {expiresIn:'100s'};
+    const cookieSetting = {
+        expires: new Date(Date.now()+ 1e5),
+        httpOnly: true,
+        secure: false
+    };
+    const token = jwt.sign(payload, process.env.JWTKEY, options);
+    res.cookie('token', token, cookieSetting).send();
+    //per testare aprire un nuovo terminal e : url -X POST localhost:5000/login
+});
+
+app.get('/user/profile', (req,res)=>{
+    const token = req.cookies.token;
+    if(!token)return res.status(401).send('Nessun token fornito');
+    try{
+        const payload = jwt.verify(token, process.env.JWTKEY);
+        console.log(payload);
+    }catch(err){ res.status(401).send('Il token non è valido, oppure è scaduto');}
+   
+   
+    res.send('Il token è valido');
+});
 
 
 
